c7da56994102923c19d47563d513e5f6403ecf1f
wang-yurin <king_1994@naver.com>
2023-04-14 15:55:24 +0900

    style: Global reset css - 자주 사용되는 태그들의 reset css

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Global, css } from '@emotion/react';
+
+const GlobalStyle = () => {
+  return (
+    <Global
+      styles={css`
+        @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+        html,
+        body,
+        div,
+        span,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        a,
+        address,
+        img,
+        strong,
+        ol,
+        ul,
+        li,
+        form,
+        label,
+        article,
+        aside,
+        footer,
+        header,
+        nav,
+        section,
+        input,
+        textarea,
+        html {
+          font-size: 62.5%;
+          font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+          body {
+            font-size: 1.6rem;
+          }
+        }
+        ol,
+        ul {
+          list-style: none;
+        }
+        a {
+          background-color: transparent;
+          text-decoration: none;
+          outline: none;
+          color: inherit;
+          &:active,
+          &:hover {
+            text-decoration: none;
+            color: inherit;
+            outline: 0;
+          }
+        }
+        button {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          outline: none;
+          border: none;
+          background: none;
+          padding: 0;
+          user-select: none;
+          cursor: pointer;
+          white-space: nowrap;
+          letter-spacing: inherit;
+          font: inherit;
+          color: inherit;
+        }
+        input {
+          outline: none;
+        }
+      `}
+    />
+  );
+};
+
+export default GlobalStyle;
